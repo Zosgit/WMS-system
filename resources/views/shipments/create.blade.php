ba7b7f3596078e8d74e17e36fe06659e126d94fe
Zosigit <zos.lat19@gmail.com>
2024-02-23 10:15:14 +0100

    shipments update

--- resources/views/shipments/create.blade.php
+++ resources/views/shipments/create.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+@section('title') {{ 'Nowa dostawa' }} @endsection
 @section('content')
 
 <div class="container">
@@ -11,92 +12,120 @@
                 </div>
                 <form action="{{ route('shipments.store') }}" class="forms-sample" method="POST">
                     @csrf
-                <div class="card-body"><canvas id="myBarChart" width="100%" height="10"></canvas>
-                    <div class="row p-2">
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="nr_doc">Numer dokumentu:</label></br>
-                                <strong>AUTONUMER</strong>
-                            </div>
-                            <div class="col-md-12 pt-4">
+
+                    <div class="card-body"><canvas id="myshipment" width="100%" height="10"></canvas>
+                        <div class="row p-1">
+                            <div class="col-md-4">
                                 <div class="form-group">
-                                    <label for="name_doc">Dokument przyjęcia</label>
-                                    <input type="text"  name="name_doc"
-                                        value="{{ isset($shipment) ? $shipment->name_doc : '' }}" class="form-control" id="name_doc" required>
+                                    <label for="code">Nr dokumentu:</label></br>
+                                    <strong>AUTONUMER</strong>
+                                </div>
+                                <div class="col-md-12 pt-2">
+                                    <div class="form-group">
+                                        <label for="code">Dokument przyjęcia</label>
+                                        <input type="text" name="external_nr"
+                                            value="" class="form-control @error('external_nr') is-invalid @enderror"
+                                             id="external_nr" required autofocus>
+
+                                            @error('external_nr')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md-8">
+                                <div class="form-group">
+                                    <label for="remarks">Komentarz</label>
+                                    <textarea type="text" name="remarks" class="form-control @error('remarks') is-invalid @enderror" id="remarks" rows="4">{{ old('remarks') }}</textarea>
+
+                                        @error('remarks')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="remarks">Komentarz</label>
-                                <textarea type="text" name="remarks"
-                                    value="{{ isset($shipment) ? $shipment->remarks : '' }}" class="form-control" id="remarks" rows="4" ></textarea>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row p-2 pt-2">
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="supplier_id">Dostawca</label>
-                                    <select name="supplier_id" class="form-select form-control @error('supplier_id') is-invalid @enderror" id="supplier_id" required>
-                                        <option value="">Wybierz</option>
-                                        @foreach ($suppliers as $supplier)
-                                        <option value="{{ $supplier->id }}"
-                                            @if (isset($shipment))
-                                            {{ $shipment->supplier_id == $supplier->id ? 'selected' : '' }}
-                                    @endif>
-                                    {{ $supplier->code }}</option>
-                                    @endforeach
 
-                                        @error('supplier_id')
+                        <div class="row p-2 pt-3">
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="firm_id">Dostawca</label>
+                                    <select name="firm_id" class="select_2 select2-container form-control @error('firm_id') is-invalid @enderror"  id="firm_id" required>
+                                        <option value="">Wybierz</option>
+                                        @foreach ($firms as $firm)
+                                            <option value="{{ $firm->id }}" @selected(old('firm_id') == $firm)>
+                                                {{ $firm->code }}
+                                            </option>
+                                        @endForeach
+                                        @error('firm_id')
                                             <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $message }}</strong>
                                             </span>
                                         @enderror
                                     </select>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="holder_id">Właściciel</label>
-                                    <select name="holder_id" class="form-select form-control @error('holder_id') is-invalid @enderror" id="holder_id" required>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="owner_id">Właściciel</label>
+                                    <select name="owner_id" class="select_2 form-select form-control @error('owner_id') is-invalid @enderror" id="owner_id" required>
                                         <option value="">Wybierz</option>
-                                        @foreach ($holders as $holder)
-                                        <option value="{{ $holder->id }}"
-                                            @if (isset($shipment))
-                                            {{ $shipment->holder_id == $holder->id ? 'selected' : '' }}
-                                    @endif>
-                                    {{ $holder->code }}</option>
-                                    @endforeach
-
-                                        @error('holder_id')
+                                        @foreach ($owners as $owner)
+                                            <option value="{{ $owner->id }}" @selected(old('owner_id') == $owner)>
+                                                {{ $owner->code }}
+                                            </option>
+                                        @endForeach
+                                        @error('owner_id')
                                             <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $message }}</strong>
                                             </span>
                                         @enderror
                                     </select>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="location_id">Miejsce</label>
+                                    <select name="location_id" class="select_2 form-select form-control @error('location_id') is-invalid @enderror" id="location_id" required>
+                                        <option value="">Wybierz</option>
+                                        @foreach ($locations as $location)
+                                            <option value="{{ $location->id }}" @selected(old('location_id') == $location)>
+                                                {{ $location->ean }}
+                                            </option>
+                                        @endForeach
+                                        @error('location_id')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </select>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div class="col-md-6 p-3">
-                        <button type="submit" class="btn btn-primary mr-4">Potwierdź</button>
-                        <a href="{{ route('shipments.index') }}" class="btn btn-light">Anuluj</a>
+                        <div class="col-md-6 p-3">
+                            <button type="submit" class="btn btn-primary mr-4">Potwierdź</button>
+                            <a href="{{ route('shipments.index') }}"
+                                    class="btn btn-light">Anuluj</a>
+                        </div>
                     </div>
-                </div>
-            </form>
-
-            @push('scripts')
-                <script src="{{ asset("plugins/select2/dist/js/select2.min.js")}}"></script>
-                <script>
-                    $(document).ready(function() {
-                    $('.select_2').select2();
-                    });
-                </script>
-            @endpush
+                </form>
+                    @push('scripts')
+                        <script src="{{ asset("plugins/select2/dist/js/select2.min.js")}}"></script>
+                        <script>
+                            $(document).ready(function() {
+                            $('.select_2').select2();
+                            });
+                        </script>
+                    @endpush
 
             </div>
         </div>
     </div>
+
 </div>
 @endsection
--- resources/views/shipments/index.blade.php
+++ resources/views/shipments/index.blade.php
@@ -1,39 +1,82 @@
 @extends('layouts.app')
+@section('title') {{ 'Lista dostaw' }} @endsection
 @section('content')
 
     <div class="card mb-4">
-
+        <div class="card-header">
+            <b>Lista dostaw</b>
+                <a href="{{ route('shipments.create') }}"
+                    class="btn btn-primary float-end px-4 btn-sm">Dodaj</a>
+        </div>
         <div class="card-body">
-            <div class="table-responsive-sm table-striped mt-2">
-                <table class="table table-striped ">
-                    <thead>
+            <form action="" method="GET">
+                <div class="row mb-2">
+                    <div class="col-md-2">
+                        <select name="type" class="form-select">
+                            <option value="">Wybierz pole</option>
+                            <option value="ship_nr">Nr dostawy</option>
+                            <option value="external_nr">Dok zewnętrzny</option>
+                            <option value="code_firm">Dostawca</option>
+                            <option value="code_owner">Właściciel</option>
+                            <option value="code_status">Status</option>
+                            <option value="location">Miejsce</option>
+                        </select>
+                    </div>
+                    <div class="col-md-3">
+                        <input type="text" name="search" value="" class="form-control" placeholder="Wprowadź dane..."/>
+                    </div>
+                    <div class="col-md-5">
+                        <button type="submit" class="btn btn-dark">Filtruj</button>
+                    </div>
+                </div>
+            </form>
+            <table id="data_table" class="table table-striped table-hover table-sm">
+                <thead>
                     <tr>
-                        <th>Indeks</th>
-                        <th>Numer dokumentu</th>
-                        <th>Data wystawienia</th>
+                        <th>Dokument dostawy</th>
+                        <th>Numer zewnątrzny</th>
+                        <th>Dostawca</th>
+                        <th>Właściciel</th>
                         <th>Status</th>
+                        <th>Miejsce</th>
+                        <th>Data</th>
                         <th> </th>
                     </tr>
                     </thead>
                     <tbody>
                         @foreach($shipments as $shipment)
-                        <tr>
-                            <td>{{ $shipment->id }}</td>
-                            <td>{{ $shipment->nr_doc }}</td>
-                            <td>{{ $shipment->created_at }}</td>
-                            <td>{{ $shipment->status->code }}</td>
-                            <td><a href="{{ route('shipmentdetail.show',['shipment'=>$shipment])}}">
-                                <svg class="icon icon-lg">
-                                    <use xlink:href="{{ asset('icons/coreui.svg#cil-arrow-right') }}"></use>
-                                </svg>
-                            </a>
-                        </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
+                            <tr>
+                                <td>{{ $shipment->ship_nr }}</td>
+                                <td>{{ $shipment->external_nr}}</td>
+                                <td>{{ $shipment->code_firm}}</td>
+                                <td>{{ $shipment->code_owner}}</td>
+                                <td>{{ $shipment->code_status}}</td>
+                                <td>{{ $shipment->location ?? ''}}</td>
+                                <td>{{ $shipment->updated_at}}</td>
+                                <td><a href="{{ route('shipmentdetail.index',['id'=>$shipment->id])}}">
+                                    <svg class="icon icon-lg">
+                                        <use xlink:href="{{ asset('icons/coreui.svg#cil-arrow-right') }}"></use>
+                                    </svg>
+                                </a>
+                            </td>
+                            </tr>
+                        @endforeach
+                </tbody>
+            </table>
+            <div>
+                {{$shipments->links() }}
             </div>
+            <p>
+                Ilość: {{$shipments->count()}} z {{ $shipments->total() }} rekordów.
+            </p>
+
         </div>
+
+        {{--
+        <div class="card-footer">
+            {{ $users->links() }}
+        </div>
+        --}}
     </div>
 
 @endsection
