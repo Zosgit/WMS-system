0a8adfa1ff60e2da98bf5c21f1bbd20b0f9f548c
Zosigit <zos.lat19@gmail.com>
2024-02-23 10:13:54 +0100

    shipmentcontrol create

--- resources/views/shipmentcontrol/close.blade.php
+++ resources/views/shipmentcontrol/close.blade.php
@@ -3,14 +3,13 @@
     <div class="modal-dialog modal-dialog-centered" role="document">
       <div class="modal-content">
         <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLongTitle">{{ $shipmentdetail->product->code }}</h5>
+            <h5 class="modal-title" id="exampleModalLongTitle">{{ $shipmentdetail->prod_code }}</h5>
             <button type="button" class="btn-close" data-coreui-dismiss="modal" aria-label="Close"></button>
         </div>
-        <div class="modal-body">Czy na pewno chcesz usunąć?
-                <form id="delete-data" action="{{ route('shipmentdetail.destroy'['shipment' => $shipment, 'shipmentdetail' => $shipmentdetail]) }}" method="POST" class="d-none">
-                    @method('Delete')
+        <div class="modal-body">Czy na pewno chcesz zakończyć?
+                <form id="delete-data" action="{{ route('control.closepos', $shipmentdetail->id) }}" method="POST" class="d-none">
+                    @method('Post')
                     @csrf
-                    <label for="" class="text-center">Are you sure you want to delete this?</label>
                 </div>
                     <div class="modal-footer">
                         <button type="submit" class="btn btn-danger px-4 btn-sm">Tak, usuń</button>
