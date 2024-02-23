da7c2a000d78077e56ba4ef5b089fee97c8225b0
Zosigit <zos.lat19@gmail.com>
2024-02-23 10:20:05 +0100

    dashboard update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,11 +2,11 @@
 
 <div class="row">
 
-    <div class="col-sm-6 col-lg-3">
+    <div class="col-sm-6 col-lg-4">
         <div class="card mb-4 text-white bg-primary" >
           <div class="card-body pb-0 d-flex justify-content-between align-items-start">
             <div>
-              <div class="fs-4 fw-semibold">15% <span class="fs-6 fw-normal"> (-12.4%
+              <div class="fs-4 fw-semibold">19% <span class="fs-6 fw-normal"> (-12.4%
                   <svg class="icon">
                     <use xlink:href="{{ asset('icons/coreui.svg#cil-arrow-bottom') }}"></use>
                   </svg>)</span></div>
@@ -16,21 +16,21 @@
           </div>
           <div class=" mt-3 mx-3" style="height:30px;">
             <div class="progress progress-white progress-thin my-2">
-                <div class="progress-bar" role="progressbar" style="width: 15%" aria-valuenow="15" aria-valuemin="0" aria-valuemax="100"></div>
+                <div class="progress-bar" role="progressbar" style="width: 15%" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
         </div>
         </div>
     </div>
 
-    <div class="col-sm-6 col-lg-3">
+    <div class="col-sm-6 col-lg-4">
         <div class="card mb-4 text-white bg-info" >
           <div class="card-body pb-0 d-flex justify-content-between align-items-start">
             <div>
-              <div class="fs-4 fw-semibold">35% <span class="fs-6 fw-normal"> (-12.4%
+              <div class="fs-4 fw-semibold">35% <span class="fs-6 fw-normal"> (-0.4%
                   <svg class="icon">
                     <use xlink:href="{{ asset('icons/coreui.svg#cil-arrow-bottom') }}"></use>
                   </svg>)</span></div>
-              <div>Strefa wysyłki</div>
+              <div>Strefa kompletacji</div>
             </div>
           </div>
           <div class=" mt-3 mx-3" style="height:30px;">
@@ -41,35 +41,16 @@
         </div>
     </div>
 
-      <div class="col-sm-6 col-lg-3">
+
+    <div class="col-sm-6 col-lg-4">
         <div class="card mb-4 text-white bg-warning">
           <div class="card-body pb-0 d-flex justify-content-between align-items-start">
             <div>
-              <div class="fs-4 fw-semibold">55% <span class="fs-6 fw-normal"> (-12.4%
+              <div class="fs-4 fw-semibold">75% <span class="fs-6 fw-normal"> (+23.9%
                   <svg class="icon">
-                    <use xlink:href="{{ asset('icons/coreui.svg#cil-arrow-bottom') }}"></use>
+                    <use xlink:href="{{ asset('icons/coreui.svg#cil-arrow-top') }}"></use>
                   </svg>)</span></div>
-              <div>Strefa składowania</div>
-            </div>
-
-          </div>
-          <div class=" mt-3 mx-3" style="height:30px;">
-            <div class="progress progress-white progress-thin my-2">
-                <div class="progress-bar" role="progressbar" style="width: 55%" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-        </div>
-        </div>
-      </div>
-
-    <div class="col-sm-6 col-lg-3">
-        <div class="card mb-4 text-white bg-danger">
-          <div class="card-body pb-0 d-flex justify-content-between align-items-start">
-            <div>
-              <div class="fs-4 fw-semibold">75% <span class="fs-6 fw-normal"> (-12.4%
-                  <svg class="icon">
-                    <use xlink:href="{{ asset('icons/coreui.svg#cil-arrow-bottom') }}"></use>
-                  </svg>)</span></div>
-              <div>Strefa kompletacji</div>
+              <div>Strefa wysyłki</div>
             </div>
 
           </div>
