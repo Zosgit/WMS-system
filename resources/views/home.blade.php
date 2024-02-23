bb6e3057090d97ff250f8f49b8066159834a0395
Zosigit <zos.lat19@gmail.com>
2024-02-23 10:21:17 +0100

    home update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.app')
-
+@section('title') {{ 'Strona główna' }} @endsection
 
 @section('content')
     @include('dashboard')
     <div class="card mb-4">
         <div class="card-header">
-          <strong>  {{ __('Dashboard') }} </strong>
+          <strong>  Zapas  magazynu </strong>
         </div>
         <div class="card-body">
-            {{ __('You are logged in!') }}
+
         </div>
     </div>
 @endsection
